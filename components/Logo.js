3891ea3361c67b25794f6c22af5c2a27f95c7abd
Zinni Lai <fk2946@yahoo.com.tw>
2023-08-15 19:23:00 +0800

    Update Logo.js

--- components/Logo.js
+++ components/Logo.js
@@ -4,6 +4,7 @@ export default function Logo() {
   return (
     <Link href={"/"} className="flex  gap-1.5">
       <img
+        className="w-6 h-6"
         src="https://test-for-aws-course-only.s3.ap-southeast-2.amazonaws.com/icons8-bird-48+(1).png"
         alt="logo"
       />
