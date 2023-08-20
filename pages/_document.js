23e0a0af75139c678d40ecdf7523e8458abdeeb1
Zinni Lai <fk2946@yahoo.com.tw>
2023-08-20 16:40:24 +0800

    add icon

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="https://test-for-aws-course-only.s3.ap-southeast-2.amazonaws.com/icons8-bird-48+(1).png"
+        />
+      </Head>
 
       <body>
         <Main />
